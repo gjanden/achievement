d703301810b1eefb05f1816657955b25e609dbeb
gjanden <gjanden@nudle.co>
2014-08-18 15:50:18 -0600

    fixing ports

--- static/js/services/registerService.js
+++ static/js/services/registerService.js
@@ -1,6 +1,6 @@
 'use strict';
  angular.module('achievementApp').factory('registration', function($resource){
-  return $resource('http://localhost:3000/users/:_id',{_id: '@_id'},{
+  return $resource('/users/:_id',{_id: '@_id'},{
    get: {method:'GET', isArray: true},
    add: {method:'POST'},
    delete: {method: 'DELETE'}
--- static/js/services/textService.js
+++ static/js/services/textService.js
@@ -3,10 +3,10 @@ var app = angular.module('achievementApp');
 
 app.service('textService', function ($http){ 
 this.getData = function(){ 
-return $http.get('http://localhost:3000/sendtext'); 
+return $http.get('/sendtext'); 
 }; 
 
 this.postData = function(data){ 
-return $http.post('http://localhost:3000/sendtext', data); 
+return $http.post('/sendtext', data); 
 }; 
 }); 
